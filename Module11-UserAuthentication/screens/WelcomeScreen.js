b0f908208e351940d755767769f8c06059896cd9
gokcanparlaktas <gokcanparlaktas@hotmail.com>
2026-04-24 05:14:49 +0300

    Module 11 - Completed / Auth with Firebase

--- Module11-UserAuthentication/screens/WelcomeScreen.js
+++ Module11-UserAuthentication/screens/WelcomeScreen.js
@@ -1,10 +1,27 @@
+import axios from "axios";
+import { useContext, useEffect, useState } from "react";
 import { StyleSheet, Text, View } from "react-native";
+import { AuthContext } from "../store/auth-context";
 
 function WelcomeScreen() {
+  const [welcomeMessage, setWelcomeMessage] = useState("");
+  const authCtx = useContext(AuthContext);
+  const token = authCtx.token;
+  useEffect(() => {
+    axios
+      .get(
+        "https://my-react-native-training-default-rtdb.europe-west1.firebasedatabase.app/message.json?auth=" +
+          token,
+      )
+      .then((response) => {
+        setWelcomeMessage(response.data);
+      });
+  }, []);
   return (
     <View style={styles.rootContainer}>
       <Text style={styles.title}>Welcome!</Text>
       <Text>You authenticated successfully!</Text>
+      <Text>{welcomeMessage}</Text>
     </View>
   );
 }
